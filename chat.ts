f8059df026a0c60c5e671064e929195ae34edca4
Jim Huang <jim.huang@glu.com>
2023-03-05 15:49:49 -0800

    add option to enable streaming and continue chat

--- chat.ts
+++ chat.ts
@@ -5,10 +5,21 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 class ChatGPTClient {
-  constructor(promptTemplate, apiKey, model, temperature, top_p) {
+  constructor(
+    promptTemplate, 
+    apiKey, 
+    model, 
+    temperature, 
+    top_p, 
+    enableStreaming, 
+    continueChat
+  ) {
     this.buffer = "";
     this.promptTemplate = promptTemplate;
     this.parentMessageId = null;
+    this.enableStreaming = enableStreaming;
+    this.continueChat = continueChat;
+
     this.api = new ChatGPTAPI({
       apiKey,
       completionParams: {
@@ -31,11 +42,27 @@ class ChatGPTClient {
   async sendMessage(messageToChatGPT) {
     this.buffer = "";
     process.stdout.write("chatgpt: ");
-    const res = await this.api.sendMessage(messageToChatGPT, {
+    let options = {
       parentMessageId: this.parentMessageId,
       onProgress: this.handleProgress.bind(this)
-    });
+    };
+
+    if (!this.enableStreaming) {
+      options = {
+        parentMessageId: this.parentMessageId,
+      }
+    }
+
+    const res = await this.api.sendMessage(messageToChatGPT, options);
+    if (!this.enableStreaming) {
+      process.stdout.write(res.text);
+    }
     process.stdout.write("\n\n");
+
+    if (!this.continueChat) {
+      process.exit(1);
+    }
+
     process.stdout.write("me: ");
     this.parentMessageId = res.id;
   }
@@ -57,6 +84,8 @@ const apiKey = process.env.OPENAI_API_KEY;
 const model = "gpt-3.5-turbo";
 const temperature = 0.5;
 const top_p = 0.8;
+const enableStreaming = process.argv[3] && process.argv[3] == 0 ? false : true;
+const continueChat = process.argv[4] && process.argv[4] == 0 ? false : true;
 
-const client = new ChatGPTClient(process.argv[2] || 'prompts/default.txt', apiKey, model, temperature, top_p);
+const client = new ChatGPTClient(process.argv[2] || 'prompts/default.txt', apiKey, model, temperature, top_p, enableStreaming, continueChat);
 client.init();
